1879e3678b8ce4a8e5eeafec0dbff05a0d96452d
Matthew Ballance <matt.ballance@gmail.com>
2022-05-17 19:40:46 -0700

    XX
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- test/unit/test_discovery.py
+++ test/unit/test_discovery.py
@@ -5,11 +5,39 @@ Created on May 17, 2022
 '''
 from unittest.case import TestCase
 from tbgen.template_rgy import TemplateRgy
+import sys
+from uvmf_yaml_utils.uvmf_core.yaml2uvmf import DataClass
+from test_base import TestBase
+import traceback
+from uvmf_yaml_utils.library import Library
+from uvmf_yaml_utils.gen_spec import GenSpec
 
-class TestDiscovery(TestCase):
+class TestDiscovery(TestBase):
     
     def test_smoke(self):
         rgy = TemplateRgy.inst()
         
+        env1 = self.createFile("env1.yaml", 
+        """
+        uvmf:
+          environments:
+            env1: {}
+        """)        
+        
         print("Templates: %s" % str(rgy.templates))
         print("Path: %s" % rgy.templates[0].path)
+        
+        lib = Library()
+        lib.addPath(env1)
+        
+        lib.load()
+        
+        spec = GenSpec(
+            template_path='/project/fun/uvmf/pytbgen-templates-pyuvm/src/tbgen_templates_pyuvm/share/template_files',
+            outdir="my_bench"
+            )
+        
+        lib.generate(spec)
+
+        print("sys.argv=%s" % str(sys.argv))
+        
